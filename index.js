e4ff13959efcecfc12c1896cbfc4dbb9388edd87
Lucas Reis <lucaskreis.com>
2022-04-04 12:50:10 -0300

    banco de dados, estrutura de classes e início do dom

---
+++ index.js
@@ -0,0 +1,13 @@
+    const btnFlags = document.getElementById('btnFlags');
+    const btnMovies = document.getElementById('btnMovies');
+    const bandeiras = document.getElementById('bandeiras');
+    const filmes = document.getElementsByTagName('filmes');
+
+    let data = require("./data");
+
+    const jogo = new jogo (flags,movies);
+
+    btnFlags.addEventListener('click', () => {
+        bandeiras
+        
+    })
---
+++ jogo.js
@@ -0,0 +1,40 @@
+let data = require("./data");
+
+class jogo {
+    constructor(flags, movies){
+        this.bandeiras = flags;
+        this.filmes = movies;
+        
+
+    }
+
+    embaralhadorBandeiras(){
+     const position = []
+
+     for(let i = 0; i < this.bandeiras.imagem.length; i++) {
+
+     }
+        
+
+    }
+
+    embaralhadorFilmes(){
+    // seleciona 9 imgs aleatórias de filmes
+
+    }
+
+    corretorBandeiras() {
+    // checa se o input está correto
+
+    }
+
+    corretorBandeiras() {
+    // checa se o input está correto
+    
+    }
+
+    telaFinal() {
+    // mostra a tela final com o resultado
+
+    }
+}
