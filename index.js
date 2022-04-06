c459c9e1c9967c44e5f8c51f5f86fad57f16047b
Lucas Reis <lucaskreis.com>
2022-04-06 17:57:19 -0300

    MVP

--- index.js
+++ index.js
@@ -4,17 +4,70 @@
     const filmes = document.getElementById('filmes');
     const flagImg = document.getElementsByClassName('flagImg');
     const movieImg = document.getElementsByClassName('movieImg');
-    const input = document.getElementsByClassName('send');
-    const inputAnswers = document.getElementsByClassName("name");
+    const grid1 = document.getElementById('grid1');
+   const btn1 = document.getElementsByClassName('btn1');
+   const text1 = document.getElementsByClassName('text1');
+ 
 
-    //let data = require("./data");
 
     const game = new jogo (flags, movies);
 
     btnFlags.addEventListener('click', () => {
         
         bandeiras.classList.replace("bandeirasOff" , "bandeirasOn");
+        for (let i = 0; i < 9; i++){
+
+        const divBloco1 = document.createElement('div')
+        divBloco1.setAttribute("class", "bloco1")
+    
+        const img = document.createElement('img')
+        img.setAttribute("src", "insira a imagem aqui")
+        img.setAttribute("alt", "imagem aleatória de bandeira")
+        img.setAttribute("class", "flagImg")
+        img.setAttribute("alt", "tag")
         
+        const form1 = document.createElement('form')
+
+        const inputText = document.createElement("input")
+        inputText.setAttribute("type", "text")
+        inputText.setAttribute("name", "name")
+        inputText.setAttribute("class", "text1")
+
+        const inputSubmit = document.createElement("input")
+        inputSubmit.setAttribute("type" , "submit")
+        inputSubmit.setAttribute("value" , "Enviar")
+        inputSubmit.setAttribute("class" , "btn1")
+
+        divBloco1.appendChild(img)
+        divBloco1.appendChild(form1)
+        grid1.appendChild(divBloco1)
+        form1.appendChild(inputText)
+        form1.appendChild(inputSubmit)
+        
+        console.log(img)
+        let counter1 = 0
+        inputSubmit.addEventListener('click', (e) => {
+            e.preventDefault()
+            
+            if(counter1 >=2){
+                alert("Sabe de nada!! Suas chances terminaram.")
+            }
+            else if(counter1 === 1){
+                alert("Béééé, errado! Você só tem mais uma chance")
+                const value1 = inputText.value;
+                console.log(value1);
+                game.corretorBandeiras(value1,img)
+                counter1++
+            }
+            else{
+             const value1 = inputText.value;
+             console.log(value1);
+             game.corretorBandeiras(value1,img)
+             counter1++
+            }
+     })
+
+    }
         game.embaralhadorBandeiras(flagImg)
 
     })
@@ -27,29 +80,7 @@
 
     })
 
-    //input.addEventListener('click', (e) => {
-   //     e.preventDefault()
-    
-    //    const name = document.getElementsByClassName('name');
-      //  const value = name.value;
-        //console.log(value);
-    //})
-        for (let i = 0; i < input.length; i++){
+  
 
-        input[i].addEventListener("click", (e) => {
-            e.preventDefault()
-
-            let value = inputAnswers[i].value
-
-            game.corretorBandeiras(value)
-                      
-            // Comparando a resposta dada naquele input com a array de respostas certas pelo index
-            //if (inputAnswers[i].value === game.deck[i].tag) {
-                console.log(value)
-            //} else {
-                // Se tiver errado cai aqui
-           // }
-        })
+   
     
-    
-    }
--- jogo.js
+++ jogo.js
@@ -1,5 +1,3 @@
-//let data = require("./data");
-
 class jogo {
     constructor(flags, movies){
         this.bandeiras = flags;
@@ -11,38 +9,45 @@ class jogo {
     }
 
     embaralhadorBandeiras(card){
-
+     //let acervo =[]
         for(let i = 0; i < 9; i++) {
 
-            const idx = Math.floor(Math.random()*this.bandeiras.length);
-            card[i].setAttribute('src',this.bandeiras[idx].imagem)
-            
-            this.deckFlags.push(this.bandeiras[idx]);
-        }
+            const idx = Math.floor(Math.random()*this.bandeiras.length);  
+           
+            //acervo.push(idx)
+            //if(acervo.indexOf[idx] === -1){
         
-         
-    }
-
+            card[i].setAttribute('src',this.bandeiras[idx].imagem);
+            card[i].setAttribute('alt',this.bandeiras[idx].tag);
+            //this.deckFlags.push(this.bandeiras[idx]);
+            }
+        }
+                        
+            //console.log(acervo)
+    
+      
+        
     embaralhadorFilmes(card){
-
+        
         for(let i = 0; i < 9; i++) {
        
             const idx = Math.floor(Math.random()*this.filmes.length);
-            console.log(idx)
+            
             card[i].setAttribute('src',this.filmes[idx].frame)
             this.deckMovies.push(this.filmes[idx]);
             }    
            }
 
-    corretorBandeiras(resposta) {
-        let counter = 0
-        for(let i = 0; i < this.deckFlags.length; i++) {
-        if (resposta === this.deckFlags[i].tag){
+    corretorBandeiras(value1, img) {
+        let counter = 0;
+
+        if(value1 === img.alt){
             counter++
-            console.log(counter) 
-        }
 
-    }}
-
+            console.log("correto")}
+        
+        
+     
     
-}
+    }
+ }
