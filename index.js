22a7ba0224d917642774202c6312c37a89f0bbc4
Lucas Reis <lucaskreis.com>
2022-04-07 09:12:50 -0300

    almost done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
     const flagImg = document.getElementsByClassName('flagImg');
     const movieImg = document.getElementsByClassName('movieImg');
     const grid1 = document.getElementById('grid1');
+    const grid2 = document.getElementById('grid2');
    const btn1 = document.getElementsByClassName('btn1');
    const text1 = document.getElementsByClassName('text1');
  
@@ -31,6 +32,8 @@
         const inputText = document.createElement("input")
         inputText.setAttribute("type", "text")
         inputText.setAttribute("name", "name")
+        inputText.setAttribute("class", "text1green")
+        inputText.setAttribute("class", "text1red")
         inputText.setAttribute("class", "text1")
 
         const inputSubmit = document.createElement("input")
@@ -51,20 +54,23 @@
             
             if(counter1 >=2){
                 alert("Sabe de nada!! Suas chances terminaram.")
+                img.setAttribute("src", "./assets/images/looser.jpg" )
             }
-            else if(counter1 === 1){
+            /*else if(counter1 === 1){
                 alert("Béééé, errado! Você só tem mais uma chance")
                 const value1 = inputText.value;
                 console.log(value1);
                 game.corretorBandeiras(value1,img)
                 counter1++
-            }
+            }*/
             else{
              const value1 = inputText.value;
-             console.log(value1);
-             game.corretorBandeiras(value1,img)
+             
+             game.corretorBandeiras(value1,img,inputText)
              counter1++
+             img.setAttribute("src", "./assets/images/sabedenada.jpg" )
             }
+            console.log(counter1)
      })
 
     }
@@ -75,11 +81,71 @@
     btnMovies.addEventListener('click', () => {
         
         filmes.classList.replace("filmesOff" , "filmesOn");
+        for (let i = 0; i < 9; i++){
 
+        const divBloco2 = document.createElement('div')
+        divBloco2.setAttribute("class", "bloco2")
+    
+        const img = document.createElement('img')
+        img.setAttribute("src", "insira a imagem aqui")
+        img.setAttribute("alt", "imagem aleatória de bandeira")
+        img.setAttribute("class", "movieImg")
+        img.setAttribute("alt", "tag")
+        
+        const form1 = document.createElement('form')
+
+        const inputText = document.createElement("input")
+        inputText.setAttribute("type", "text")
+        inputText.setAttribute("name", "name")
+        
+        inputText.setAttribute("class", "text1green")
+        inputText.setAttribute("class", "text1red")
+        inputText.setAttribute("class", "text1")
+
+        const inputSubmit = document.createElement("input")
+        inputSubmit.setAttribute("type" , "submit")
+        inputSubmit.setAttribute("value" , "Enviar")
+        inputSubmit.setAttribute("class" , "btn1")
+
+        divBloco2.appendChild(img)
+        divBloco2.appendChild(form1)
+        grid2.appendChild(divBloco2)
+        form1.appendChild(inputText)
+        form1.appendChild(inputSubmit)
+        
+        console.log(img)
+
+        let counter1 = 0
+        inputSubmit.addEventListener('click', (e) => {
+            e.preventDefault()
+            
+            if(counter1 >=2){
+                alert("Sabe de nada!! Suas chances terminaram.")
+                img.setAttribute("src", "./assets/images/looser.jpg" )
+            }
+            /*else if(counter1 === 1){
+                alert("Béééé, errado! Você só tem mais uma chance")
+                const value1 = inputText.value;
+                console.log(value1);
+                game.corretorBandeiras(value1,img)
+                counter1++
+            }*/
+            else{
+             const value1 = inputText.value;
+             
+             game.corretorFilmes(value1,img,inputText)
+             counter1++
+             if (value1===img.alt){
+             img.setAttribute("src", "./assets/images/sabedenada.jpg" )
+            }
+            }
+            console.log(counter1)
+     })
+
+    }
         game.embaralhadorFilmes(movieImg)
 
     })
-
   
 
    
--- jogo.js
+++ jogo.js
@@ -2,52 +2,68 @@ class jogo {
     constructor(flags, movies){
         this.bandeiras = flags;
         this.filmes = movies;
-        this.deckFlags = [];
-        this.deckMovies = [];
-        
-
     }
 
     embaralhadorBandeiras(card){
-     //let acervo =[]
+    
         for(let i = 0; i < 9; i++) {
 
             const idx = Math.floor(Math.random()*this.bandeiras.length);  
-           
-            //acervo.push(idx)
-            //if(acervo.indexOf[idx] === -1){
-        
+                   
             card[i].setAttribute('src',this.bandeiras[idx].imagem);
             card[i].setAttribute('alt',this.bandeiras[idx].tag);
             //this.deckFlags.push(this.bandeiras[idx]);
+            this.bandeiras.splice(idx , 1);
             }
         }
-                        
-            //console.log(acervo)
-    
-      
         
     embaralhadorFilmes(card){
         
         for(let i = 0; i < 9; i++) {
        
             const idx = Math.floor(Math.random()*this.filmes.length);
-            
             card[i].setAttribute('src',this.filmes[idx].frame)
-            this.deckMovies.push(this.filmes[idx]);
+            card[i].setAttribute('alt',this.filmes[idx].tag);
+            //this.deckMovies.push(this.filmes[idx]);
+            this.filmes.splice(idx , 1);
             }    
-           }
+        }
 
-    corretorBandeiras(value1, img) {
+    corretorBandeiras(value1, img, text1) {
         let counter = 0;
 
         if(value1 === img.alt){
-            counter++
+            text1.setAttribute("class", "text1green")
 
             console.log("correto")}
-        
-        
-     
-    
+        else {
+            counter++
+            if(counter === 1){
+                text1.setAttribute("class", "text1red")
+            }
+            else if(counter ===2){
+                text1.remove()
+            }
+        }    
     }
+
+    corretorFilmes(value1, img, text1) {
+        let counter = 0;
+
+        if(value1 === img.alt){
+            text1.setAttribute("class", "text1green")
+
+            console.log("correto")}
+        else {
+            counter++
+            if(counter === 1){
+                text1.setAttribute("class", "text1red")
+            }
+            else if(counter ===2){
+                text1.remove()
+            }
+        }    
+    }
+
+
  }
